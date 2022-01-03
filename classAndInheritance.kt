f6ceb5d0865d2c2a3870e0082dea007ac95e3ce2
Rakesh Bhardwaj <57101915+RakeshBhardwaj-coder@users.noreply.github.com>
2022-01-03 20:07:47 +0530

    improved class&Inheritace with some extra code

--- classAndInheritance.kt
+++ classAndInheritance.kt
@@ -1,9 +1,23 @@
 fun main(){
     val squareCabin = SquareCabin(6)
+    val roundHut = RoundHut(2)
+    val roundTower = RoundTower(4)
     
     with(squareCabin){
         
     println("\nSquare Cabin\n============")
+    println("Capacity: ${capacity}")
+    println("Material: ${buildingMaterial}")
+    println("Has room? ${hasRoom()}")
+    }
+    with(roundHut){
+    println("\nRound Hut\n============")
+    println("Capacity: ${capacity}")
+    println("Material: ${buildingMaterial}")
+    println("Has room? ${hasRoom()}")
+    }
+     with(roundTower){
+    println("\nRound Tower\n============")
     println("Capacity: ${capacity}")
     println("Material: ${buildingMaterial}")
     println("Has room? ${hasRoom()}")
@@ -23,9 +37,33 @@ class SquareCabin(residence: Int): Drwelling(residence){
     override val capacity = 8
 }
 
-// Output
-//      Square Cabin
-//      ============
-//      Capacity: 8
-//      Material: Wood
-//      Has room? true
+// open is used because the RoundHut is 'final' by default and used it in RoundTower
+
+open class RoundHut(residence: Int): Drwelling(residence){
+    override val buildingMaterial = "Straw"
+    override val capacity = 4
+}
+ class RoundTower(residents: Int, floor: Int = 2) : RoundHut(residents) {
+    override val buildingMaterial = "Stone"
+    override val capacity = 4 * floor
+}
+ 
+//  Outputs:-
+
+//  Square Cabin
+// ============
+// Capacity: 8
+// Material: Wood
+// Has room? true
+
+// Round Hut
+// ============
+// Capacity: 4
+// Material: Straw
+// Has room? true
+
+// Round Tower
+// ============
+// Capacity: 8
+// Material: Stone
+// Has room? true
